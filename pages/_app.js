e1b89f5c2084de4c6af1b2643dfe18391688b257
Jordan Cleigh <cleigh.jordan@gmail.com>
2022-10-02 19:28:37 -0500

    Add loading to routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react';
+import Router from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
-
-export default MyApp
