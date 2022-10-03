82dcd041f457bcf96810281ca37544ba6cdea412
Jordan Cleigh <cleigh.jordan@gmail.com>
2022-10-02 19:31:53 -0500

    Fixes issue #8

--- pages/repos.js
+++ pages/repos.js
@@ -7,7 +7,7 @@ export default function Repos({ repos }) {
                     <div key={`repo-${i}`}>
                         <ul>
                             <li key={`li-${item.full_name}`}>
-                                <a href={item.html_url} target="_blank">{item.full_name}</a> | Stars: {parseInt(item.stargazers_count).toLocaleString('en-US')} | Watchers: {parseInt(item.watchers_count).toLocaleString('en-US')}
+                                <a href={item.html_url} target="_blank" rel="noopener noreferrer">{item.full_name}</a> | Stars: {parseInt(item.stargazers_count).toLocaleString('en-US')} | Watchers: {parseInt(item.watchers_count).toLocaleString('en-US')}
                                 <p style={{ color: '#888', paddingLeft: '10px' }}>{item.description.substring(0, 1000)}</p>
 
                             </li>
